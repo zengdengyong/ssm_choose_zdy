54e915b973c4ec12df7486930255f7f0133ae564
zengdengyong <17628289246@163.com>
2019-01-11 12:11:04 +0800

    增加拦截器

---
+++ src/com/zdy/interceptor/AuthInterceptor.java
@@ -0,0 +1,35 @@
+package com.zdy.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("afterCompletion==========");
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("postHandle==========");
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		boolean flag = false;
+			System.out.println("拦截器拦截了");
+		return flag;
+	}
+	
+}
